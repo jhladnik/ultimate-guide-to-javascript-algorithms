b3e415035ff4b50ae9221b8cfb5a2428cb2925e0
Joe Hladnik <jhladnik20@gmail.com>
2022-10-13 00:50:16 -0500

    Add functions to reverseString START

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -5,9 +5,47 @@ E.g reverseString('algorithms') // should return 'smhtirogla'
 
 
 
+//chain in-built methods
 function reverseString(text) {
-    // Code goes here
+    return text.split("").reverse().join("");
 }
+
+function reverseString(text) {
+    return [...text].split("").reverse().join("");
+}
+
+//for loop
+function reverseString(text) {
+    let result = '';
+    for (let i = text.length-1; i>=0; i--){
+        result += text[i];
+    }
+    return result
+}
+
+//for..of loop
+function reverseString(text) {
+    let result = '';
+    for (let char of text){
+        result=char+result;
+    }
+    return result
+}
+
+//recursion
+function reverseString(text) {
+    if(text === ''){
+        return '';
+    }else{
+        return reverseString(text.substr(1)) + text[0];
+    }
+}
+
+//reduce method
+function reverseString(text) {
+    return text.split("").reduce((acc,char) => char+acc, '')
+}
+
 
 
 
